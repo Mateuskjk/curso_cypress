345c543e40412895a8bac50710058a042a1eff26
Mateuskjk <mcmateus1342@gmail.com>
2025-05-09 19:21:24 -0400

    Criando um dublê para testes

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -1,13 +1,21 @@
 describe('Página de login', () => {
     beforeEach(() =>{
-        cy.visit('https://adopet-frontend-cypress.vercel.app/');
+        cy.visit('https://adopet-frontend-cypress.vercel.app');
         cy.get('[data-test="login-button"]').click();
+        cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login',{
+            statusCode: 400, }).as('stubPost')
     })
 
     it('Validando as credenciais fornecidas no login', () => {
       cy.get('[data-test="submit-button"]').click();
       cy.contains('É necessário informar um endereço de email').should('be.visible');
       cy.contains('Insira sua senha').should('be.visible');
+    })
+
+    it('DEVE FALHAR MESMO QUE OS CAMPOS SEJAM PREENCHIDOS CORRETAMENTE', () => {
+      cy.login('ana@email.com', 'Senha123');
+      cy.wait('@stubPost')
+      cy.contains('Falha no login. Consulte suas credenciais.').should('be.visible');
     })
 
     it('Validando as Regras de négocio', () => {
